89cd592e27e889af28ca713b54eace543cc24a00
Romulo Santana <romulomsantana@hotmail.com>
2023-10-14 17:30:25 -0300

    create password hashing

---
+++ src/main/java/br/com/romulosantana/todolist/task/TaskModel.java
@@ -0,0 +1,4 @@
+package br.com.romulosantana.todolist.task;
+
+public class TaskModel {
+}
--- src/main/java/br/com/romulosantana/todolist/user/UserController.java
+++ src/main/java/br/com/romulosantana/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package br.com.romulosantana.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +17,19 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-           System.out.println("Usuário já existe");
-           return null;
+           // mensagem de erro
+            // status code
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
         }
-        return this.userRepository.save(userModel);
+
+        var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(hashedPassword);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
